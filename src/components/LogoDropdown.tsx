eab18007044807de79b01b1d022738a22a3997bd
lastborn <levinachifie016@gmail.com>
2026-07-23 14:35:52 +0300

    visible icon2

--- src/components/LogoDropdown.tsx
+++ src/components/LogoDropdown.tsx
@@ -4,7 +4,7 @@ import { Logo } from "@/components/Logo";
 export function LogoDropdown() {
   return (
     <Link to="/" className="flex items-center gap-2 shrink-0 group">
-      <Logo className="h-8 w-auto text-primary" />
+      <Logo className="h-12 w-auto text-primary" />
     </Link>
   );
 }
